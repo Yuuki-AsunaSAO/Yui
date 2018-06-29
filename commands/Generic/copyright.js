48e730ab6eee0790f259871b95c794a9958f8f4d
toman <tobi.berger13@gmail.com>
2018-06-29 20:16:49 +0200

    await syntax for copyright
    Also, something updated, maybe?

--- commands/Generic/copyright.js
+++ commands/Generic/copyright.js
@@ -12,14 +12,13 @@ module.exports = class ReplyCommand extends Command {
         });
     }
 
-    run(msg) {
-        this.client.fetchUser("365452203982323712").then(Asuna =>
-            msg.embed(
-                new Discord.RichEmbed()
-                .setDescription("\nTHE SOFTWARE IS PROVIDED 'AS IS' AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. \n\nCopyright © 2018 Asuna#3119, All rights reserved")
-                .setFooter("Asuna#3119", Asuna.avatarURL)
-                .setColor(this.client.getRandomColor())
-            )
+    async run(msg) {
+        let Asuna = await this.client.fetchUser("365452203982323712")
+        msg.embed(
+            new Discord.RichEmbed()
+            .setDescription("\nTHE SOFTWARE IS PROVIDED 'AS IS' AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. \n\nCopyright © 2018 Asuna#3119, All rights reserved")
+            .setFooter("Asuna#3119", Asuna.avatarURL)
+            .setColor(this.client.getRandomColor())
         )
     }
 };
