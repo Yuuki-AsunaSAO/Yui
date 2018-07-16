5312b7d86c108c5144407e95ba6fcfda2c59a790
toman <tobi.berger13@gmail.com>
2018-07-16 12:06:48 +0200

    .gitignore

--- commands/managing/destroy.js
+++ commands/managing/destroy.js
@@ -4,6 +4,7 @@ module.exports = class ReplyCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'destroy',
+			aliases: ['kill'],
 			group: 'managing',
 			memberName: 'destroy',
 			description: 'Kills the bot.',
